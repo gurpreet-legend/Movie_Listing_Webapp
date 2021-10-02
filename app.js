a91baf788c6c09f1914d9cbc7054a7a821c90fec
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-10-02 15:30:56 +0530

    Reverted changes

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/login', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', SignUpRouter);
 app.use('/signin', SignInRouter);
